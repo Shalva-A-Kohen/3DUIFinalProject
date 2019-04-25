ad357cc4c01c915f00e088dfc467c1c0f14782e3
Shalva-A-Kohen <sak2232@columbia.edu>
2019-04-25 17:34:27 -0400

    the cube is for testing...
    selection and manipulation work for cube

---
+++ FinalProject/Assets/Scripts/ObjectBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.XR.WSA.Input;
+using Microsoft.MixedReality.Toolkit.Input;
+using UnityEngine;
+
+public class ObjectBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var interactionSourceStates = InteractionManager.GetCurrentReading();
+        foreach (var interactionSourceState in interactionSourceStates)
+        {
+            if (interactionSourceState.selectPressed)
+            {
+                //Debug.Log("hello");
+            }
+        }
+       
+    }
+
+    void OnFocusEnter(FocusEventData eventData)
+    {
+        Debug.Log("focusing...");
+    }
+
+    void OnPointerClicked(MixedRealityPointerEventData eventData)
+    {
+        Debug.Log("pointer clicked");
+    }
+}
